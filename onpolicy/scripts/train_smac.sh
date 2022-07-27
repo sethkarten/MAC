615506ed626420043cefacdd95dd4d2bec2002f1
Seth Karten <seth.karten1@gmail.com>
2022-07-26 23:19:27 -0400

    smac t_mappo_comm params

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 env="StarCraft2"
-map="3m"
-algo="mappo"
-exp="mlp"
+map="25m"
+algo="t_mappo_comm"
+exp="t_mappo_comm"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} --n_training_threads 127 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --use_recurrent_policy
+    python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+    --map_name ${map} --seed ${seed} --n_training_threads 32 --n_rollout_threads 16 \
+    --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 \
+    --use_value_active_masks --use_eval --use_recurrent_policy
 done
