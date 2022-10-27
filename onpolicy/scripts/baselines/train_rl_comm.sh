64d6b6c7f6d425a1f46b2a3fb6156ebc0e5c7e8f
Siva <skailas@andrew.cmu.edu>
2022-10-27 07:07:22 -0400

    Initial functional Pascal VOC env

--- onpolicy/scripts/baselines/train_rl_comm.sh
+++ onpolicy/scripts/baselines/train_rl_comm.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-env="TrafficJunction"
+env="PascalVoc"
 algo="r_mappo_comm"
-difficulty="hard"
-num_agents=20
-episode_length=80
+difficulty="easy"
+num_agents=2
+episode_length=3
 exp="rl_comm"
 seed_max=1
 
@@ -11,11 +11,12 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    python train/train_traffic_junction.py --env_name ${env} \
+    python train/train_pascal_voc.py --env_name ${env} \
     --algorithm_name ${algo} --experiment_name ${exp} --difficulty ${difficulty} \
     --num_agents ${num_agents} --seed ${seed} --episode_length ${episode_length} \
-    --n_training_threads 32 --n_rollout_threads 16 --num_mini_batch 1 \
+    --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 \
     --num_env_steps 1000000 --ppo_epoch 5 --lr 1e-3 --critic_lr 1e-3 \
     --use_ReLU --gamma 1 --clip_param 0.1  --mha_comm \
-    --use_value_active_masks --hidden_size 128 --use_recurrent_policy --use_transformer_policy
+    --use_value_active_masks --hidden_size 128 --use_recurrent_policy --use_transformer_policy \
+    --cuda
 done
