Fixed numerical instability issues in mixture of GPs

onpolicy/envs/mpe/scenarios/EM_algorithm.py
@@ -22,13 +22,28 @@ def E_stage(X_train_combined, y_train_combined, GPs, P_z):
         # TODO: make this more efficient
         y_hat, var = GPs[i].query([q])
         N[j, i] = gaussian_pdf(y_hat, var, y)
+        # if np.all(np.isfinite(N[j, i])) == False:
+        #     print('NaN or Inf in N[j, i]')
+        #     print(y_hat)
+        #     print(var)
+        #     print(y)
+        #     exit()
     
     # print(N)
     
     # update P_z (as one batch)
     P_z_new = P_z.copy()
     for j, i in np.ndindex(P_z.shape):
-        P_z_new[j, i] = (P_z[j, i] * N[j, i]) / sum(P_z[j, k] * N[j, k] for k in range(n))
+        P_z_new[j, i] = (P_z[j, i] * N[j, i]) / (sum(P_z[j, k] * N[j, k] for k in range(n)) + 1e-9)
+        # if np.all(np.isfinite(P_z_new[j, i])) == False:
+        #     print('NaN or Inf in P_z_new[j, i]')
+        #     print(P_z_new[j, i])
+        #     print(P_z[j, i] * N[j, i])
+        #     print(sum(P_z[j, k] * N[j, k] for k in range(n)))
+        #     print('above are components')
+        #     print(P_z)
+        #     print(N)
+        #     exit()
     
     return P_z_new
 
@@ -39,7 +54,7 @@ def M_stage(GPs, P_z):
     for i, gp in enumerate(GPs):
         for j_local in range(len(gp.X_train)):
             # prevent divide by 0
-            gp.psi_diagonals[j_local] = gp.σ_noises[j_local] / max(P_z[j, i], 1e-9)
+            gp.psi_diagonals[j_local] = gp.σ_noises[j_local] / np.nanmax([P_z[j, i], 1e-9])
             j += 1
 
 
@@ -48,6 +63,10 @@ def EM_algorithm(X_train_combined, y_train_combined, GPs, P_z, return_P_Z_histor
     Run the EM-Algorithm until convergence.
     Modifies `GPs` in place and return updated `P_z`.
     """
+    # if np.all(np.isfinite(P_z)) == False:
+    #     print('NaN or Inf in P_z_new[j, i]')
+    #     print(P_z)
+    #     exit()
     # Run EM-Algorithm until convergence
     P_z_history = []
     epsilon = 1e-4